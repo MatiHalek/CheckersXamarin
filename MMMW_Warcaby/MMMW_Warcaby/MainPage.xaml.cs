fa08e1dd0c5fbc0541feab94391dc28259ade4a8
Mateusz Marmuźniak <mateusz.marmuzniak.poland@gmail.com>
2024-03-05 12:02:27 +0100

    Dodaj pliki projektów.

---
+++ MMMW_Warcaby/MMMW_Warcaby/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MMMW_Warcaby
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly List<Piece> Pieces = new List<Piece>();
+        private readonly Dictionary<string, List<string>> PossibleCaptures = new Dictionary<string, List<string>>();
+        private readonly Dictionary<string, List<string>> PossibleMoves = new Dictionary<string, List<string>>();
+        private string PieceSelected = null;
+        private bool IsPlaying = true;
+        private char Turn = 'w';
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MMMW_Warcaby/MMMW_Warcaby/Piece.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MMMW_Warcaby
+{
+    public class Piece
+    {
+        public ImageButton Image = new ImageButton();
+        public bool IsEnabled { get; set; } = true;
+        public Piece(string image, bool isWhite, int positionY, int positionX)
+        {
+            Image.Source = ImageSource.FromResource(image);
+            Image.ClassId = "p" + positionY + positionX + (isWhite ? "w" : "b");
+            Grid.SetRow(Image, positionY);
+            Grid.SetColumn(Image, positionX);
+        }
+    }
+}
